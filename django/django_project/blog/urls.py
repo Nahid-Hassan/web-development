a53579d39ce5c158d575f07ba5708a16c66a2bc6
Nahid-Hassan <nahid.cseru@gmail.com>
2020-12-16 15:26:39 +0600

    update

--- django/django_project/blog/urls.py
+++ django/django_project/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     # path(route, view, kwargs=None, name=None)
     path('', views.home, name='blog-home'),
---
+++ front-end-udacity/links.md
@@ -0,0 +1,1 @@
+<https://www.udacity.com/courses/all?skill=HTML&skill=CSS&skill=Flask&skill=JavaScript&skill=Neural%20Networks&skill=SQL&type=free%20courses>
